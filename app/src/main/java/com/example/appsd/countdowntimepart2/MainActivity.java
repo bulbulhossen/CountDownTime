79660fe1a9ade8bdd644d6817f79d476fe7ee2f4
Md Bulbul Hossen <bulbul.programmer.cse@gmail.com>
2016-01-26 15:27:50 +0600

    Initial commit

---
+++ app/src/main/java/com/example/appsd/countdowntimepart2/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.appsd.countdowntimepart2;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private TextView tvDay, tvHour, tvMinute, tvSecond, tvEvent;
+    private LinearLayout linearLayout1, linearLayout2;
+    private Handler handler;
+    private Runnable runnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initUI();
+        countDownStart();
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private void initUI() {
+        linearLayout1 = (LinearLayout) findViewById(R.id.ll1);
+        linearLayout2 = (LinearLayout) findViewById(R.id.ll2);
+        tvDay = (TextView) findViewById(R.id.txtTimerDay);
+        tvHour = (TextView) findViewById(R.id.txtTimerHour);
+        tvMinute = (TextView) findViewById(R.id.txtTimerMinute);
+        tvSecond = (TextView) findViewById(R.id.txtTimerSecond);
+        tvEvent = (TextView) findViewById(R.id.tvevent);
+    }
+
+    // //////////////COUNT DOWN START/////////////////////////
+    public void countDownStart() {
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                handler.postDelayed(this, 1000);
+                try {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat(
+                            "yyyy-MM-dd");
+                    // Here Set your Event Date
+                    Date futureDate = dateFormat.parse("2016-12-30");
+                    Date currentDate = new Date();
+                    if (!currentDate.after(futureDate)) {
+                        long diff = futureDate.getTime()
+                                - currentDate.getTime();
+                        long days = diff / (24 * 60 * 60 * 1000);
+                        diff -= days * (24 * 60 * 60 * 1000);
+                        long hours = diff / (60 * 60 * 1000);
+                        diff -= hours * (60 * 60 * 1000);
+                        long minutes = diff / (60 * 1000);
+                        diff -= minutes * (60 * 1000);
+                        long seconds = diff / 1000;
+                        tvDay.setText("" + String.format("%02d", days));
+                        tvHour.setText("" + String.format("%02d", hours));
+                        tvMinute.setText("" + String.format("%02d", minutes));
+                        tvSecond.setText("" + String.format("%02d", seconds));
+                    } else {
+                        linearLayout1.setVisibility(View.VISIBLE);
+                        linearLayout2.setVisibility(View.GONE);
+                        tvEvent.setText("Android Event Start");
+                        handler.removeCallbacks(runnable);
+                        // handler.removeMessages(0);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        handler.postDelayed(runnable, 0);
+    }
+
+    // //////////////COUNT DOWN END/////////////////////////
+}
